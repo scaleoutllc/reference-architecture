e648b1910f5865f174e2b8393f467ddad93e7f8d
Matt Surabian <matt@wescaleout.com>
2025-11-09 07:36:22 -0500

    Add console access

--- providers/aws-iam-accounts/iam-matt.tf
+++ providers/aws-iam-accounts/iam-matt.tf
@@ -11,3 +11,7 @@ resource "aws_iam_user_policy_attachment" "admin" {
 resource "aws_iam_access_key" "matt" {
   user = aws_iam_user.matt.name
 }
+
+resource "aws_iam_user_login_profile" "matt" {
+  user = aws_iam_user.matt.name
+}
